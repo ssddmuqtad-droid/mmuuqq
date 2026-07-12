0fe686e3900afefafee6b6de8c744333dc324ada
rameremy7-ops <rameremy7@ops.com>
2026-07-12 15:53:32 +0300

    إضافة check-migrations endpoint لفحص migrations

--- dalal_project/urls.py
+++ dalal_project/urls.py
@@ -39,6 +39,8 @@ urlpatterns = [
     path('direct-home/', lambda request: __import__('properties.views').home(request), name='direct-home'),
     # Test if properties app is loaded
     path('check-apps/', lambda request: JsonResponse({'apps': [app.name for app in __import__('django.conf').settings.INSTALLED_APPS], 'properties': 'properties' in [app.name for app in __import__('django.conf').settings.INSTALLED_APPS]}), name='check-apps'),
+    # Check migrations
+    path('check-migrations/', lambda request: JsonResponse({'migrations': __import__('django.core.management').call_command('showmigrations', 'properties', verbosity=0)}), name='check-migrations'),
     path('', include('properties.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('properties.api_urls')),
